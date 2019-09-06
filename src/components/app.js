fa76739766f226b333a33b4b9e1f464784b9048c
Rocky Singh <rockygsingh@icloud.com>
2019-09-05 23:31:36 -0400

    rendered SearchBar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import SearchBar from './searchBar'
 import ReactDOM from 'react-dom'
 
 
 const App = () => {
     return (
-        <div>Hello world</div>
+        <div>
+            <SearchBar />
+        </div>
     )
 }
 
