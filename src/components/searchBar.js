c4ef1074c4be09e6aca027a7da584b7ddb55fbc6
Rocky Singh <rockygsingh@icloud.com>
2019-09-08 18:47:39 -0400

    passed state up to app

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -13,6 +13,7 @@ class SearchBar extends React.Component {
     
     render() {
         return (
+            
          <div className="ui segment">
              <form onSubmit={ this.onFormSubmit } className="ui form">
                  <div className="field">
